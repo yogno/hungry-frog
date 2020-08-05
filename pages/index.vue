51013effd68fd59625ab754e56caa32d370ead2e
yogno <yogno.koo@gmail.com>
2020-08-05 23:26:10 +0900

    A frog eats bugs

---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        {{ welcomeMessage }}
+      </h1>
+      <div class="links">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+import Logo from "~/components/Logo.vue";
+
+@Component({
+  components: {
+    Logo
+  }
+})
+export default class extends Vue {
+  welcomeMessage: string = "Hello World"
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
